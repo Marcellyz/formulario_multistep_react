2f0f23c775b32ec218f3b91e091276a28e2febfb
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-07-12 19:16:37 -0300

    feat: persistindo dados do formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,86 @@
-
-
 //Components
-import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
-import { FiSend } from 'react-icons/fi';
-import UserForm from './components/UserForm'
-import ReviewForm from './components/ReviewForm'
-import  Thanks from './components/Thanks'
-import Steps from './components/Steps';
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
 
 // Hooks
-import { useForm } from './hooks/UseForm';
+import { useForm } from "./hooks/UseForm";
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
+const formTemplate = {
+  name: "",
+  email: "",
+  review: "",
+  comment: "",
+};
 
 function App() {
+  const [data, setData] = useState(formTemplate);
 
-  const formComponents = [<UserForm/>, <ReviewForm/>, <Thanks/>];
+  const updateFieldHandler = ( key, value) => {
+    setData((prev) => {
+      return { ...prev, [key]: value};
+    });
+  };
 
-  const {currentStep, currentComponent, changeStep, isLastStep, isFirstStep} = useForm(formComponents);
+  const formComponents = [
+    <UserForm data={data}  updateFieldHandler={updateFieldHandler} />,
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler}/>,
+    <Thanks data={data} />,
+  ];
+
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } = useForm(formComponents);
+
+  const mostrarMensagemAgradecendo = () =>{
+    alert('Obrigado, seu formulário foi enviado com sucesso!!')
+  };
 
   return (
     <>
-      <div className='app'>
+      <div className="app">
         <div className="header">
           <h2>Deixe sua avaliação</h2>
-          <p>Ficamos felizes com sua compra, utilize o formulário abaixo para avaliar o produto</p>
+          <p>
+            Ficamos felizes com sua compra, utilize o formulário abaixo para
+            avaliar o produto
+          </p>
         </div>
         <div className="form-container">
-          <Steps currentStep={currentStep}/>
+          <Steps currentStep={currentStep} />
           <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
             <div className="inputs-container">{currentComponent}</div>
             <div className="actions">
               {!isFirstStep && (
-                <button type="button" onClick={() => changeStep(currentStep - 1)}>
-                <GrFormPrevious/>
-                <span>Voltar</span>
-              </button>)}
+                <button
+                  type="button"
+                  onClick={() => changeStep(currentStep - 1)}
+                >
+                  <GrFormPrevious />
+                  <span>Voltar</span>
+                </button>
+              )}
               {!isLastStep ? (
                 <button type="submit">
-                <span>Avançar</span>
-                <GrFormNext/>
-              </button>) : (
-                <button type="button">
-                <span>Enviar</span>
-                <FiSend/>
-              </button>
+                  <span>Avançar</span>
+                  <GrFormNext />
+                </button>
+              ) : (
+                <button type="button" onClick={mostrarMensagemAgradecendo}>
+                  <span>Enviar</span>
+                  <FiSend />
+                </button>
               )}
             </div>
           </form>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
